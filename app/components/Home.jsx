d5677aa13f036b9d919ec7b7788d3a723a52c696
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-04 09:29:16 -0400

    fixed homepage and added port env option

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 function Home() {
   return (
     <div className="homepage" >
-      <h1>HOME PAGE</h1>
+      <h1 className="title is-1">Welcome to</h1>
+      <h1 className="title is-1">Zodiac University</h1>
       <img src="https://s-media-cache-ak0.pinimg.com/originals/a7/fc/e5/a7fce58ea87dd8b0c818bea2745cedff.png" />
     </div>
   );
