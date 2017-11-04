9a59b642ff9615228e0f6c9fb0f8e81df95b888b
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-04 09:46:24 -0400

    updated db index, deleted yarnlock, added heroku-postbuild script and added procfile

--- db/index.js
+++ db/index.js
@@ -4,8 +4,8 @@ const chalk = require('chalk');
 const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
-const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${name}`;
+const name = process.env.DATABASE_URL || pkg.name;
+const connectionString = `postgres://localhost:5432/${name}` || `${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
